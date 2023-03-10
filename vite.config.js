4a813514bcdfdfc1280f31ac7546eaab73b0e23b
morgane.van.velthoven <morgane.van.velthoven@student.howest.be>
2023-03-10 17:35:53 +0100

    enable https

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import fs from "fs";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/scss/app.scss",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: "myinventory.git",
+        },
+        https: {
+            key: fs.readFileSync(`./myinventory.git-key.pem`),
+            cert: fs.readFileSync(`./myinventory.git.pem`),
+        },
+    },
 });
